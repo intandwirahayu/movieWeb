bd2b90800240ddc4e1105dde12b5b533194887ee
intandwirahayu <intandwirahayu202@gmail.com>
2021-12-23 20:32:50 +0700

    change route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { Home, DetailMovie, DetailTV, Upcoming } from "./pages";
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return(
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/movieWeb" element={<Home/>}/>
         <Route path="/detailMovie/:id" element={<DetailMovie/>}/>
         <Route path="/detailTV/:id" element={<DetailTV/>}/>
         <Route path="/upcoming" element={<Upcoming/>}/>
       </Routes>
-    </BrowserRouter>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals'; 
 
 ReactDOM.render(
+  <BrowserRouter>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
